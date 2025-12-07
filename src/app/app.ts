2add9fb5ca8e259159f6040321eb63dc556edd9b
Carter Durham <w0609340@myscc.ca>
2025-12-07 03:55:36 -0500

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import {Component, ElementRef, signal, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,12 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('CDPortfolio');
+  @ViewChild('hoverSound') hoverSoundRef!: ElementRef<HTMLAudioElement>
+  playSound(): void {
+    if(this.hoverSoundRef && this.hoverSoundRef.nativeElement) {
+      this.hoverSoundRef.nativeElement.currentTime = 0;
+      this.hoverSoundRef.nativeElement.volume = 0.2;
+      this.hoverSoundRef.nativeElement.play();
+    }
+  }
 }
